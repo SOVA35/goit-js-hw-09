b64f3f63e84e5027e480017761dbf335a9537430
SOVA35 <kushnerandrew2604@gmail.com>
2023-06-23 19:23:29 +0300

    added task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let intervalID;
+
+
+startBtn.addEventListener('click', startSwichingColor);
+stopBtn.addEventListener('click', stopSwichingColor);
+
+function startSwichingColor() {
+  startBtn.disabled = true;
+  intervalID = setInterval(changeColor, 700);  
+
+}
+function stopSwichingColor() {
+  stopBtn.disabled = false;
+  clearInterval(intervalID);
+}
+function changeColor() {
+  document.body.style.background=getRandomHexColor();
+}
+
 
