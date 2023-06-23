c34ac1638b8d952669f2191cdb84166c28ffe52a
SOVA35 <121583134+SOVA35@users.noreply.github.com>
2023-06-23 16:23:59 +0000

    Deploying to gh-pages from @ SOVA35/goit-js-hw-09@b64f3f63e84e5027e480017761dbf335a9537430 🚀

---
+++ 01-color-switcher.079ba7cc.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");function a(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}e.addEventListener("click",(function(){e.disabled=!0,t=setInterval(a,700)})),n.addEventListener("click",(function(){n.disabled=!1,clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.079ba7cc.js.map
