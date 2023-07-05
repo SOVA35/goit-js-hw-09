5d0df9bc904d7a3b69b38660c67cf9d34ac55b7c
SOVA35 <121583134+SOVA35@users.noreply.github.com>
2023-07-05 12:43:52 +0000

    Deploying to gh-pages from @ SOVA35/goit-js-hw-09@6fbc73eef523a08bdc674f2a827f1862b136195c 🚀

--- 03-promises.7da809b0.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form").addEventListener("submit",(function(t){t.preventDefault();Number(e.currentTarget.delay.value)}));
-//# sourceMappingURL=03-promises.7da809b0.js.map
--- 03-promises.97ec68dd.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form").addEventListener("submit",(function(t){t.preventDefault(),Number(e.currentTarget.delay.value)}));
-//# sourceMappingURL=03-promises.97ec68dd.js.map
